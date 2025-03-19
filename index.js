88651aaf5a9ee835e6f21036509c34513ba5c233
hackwellbn <119762901+hackwellbn@users.noreply.github.com>
2025-03-19 14:35:40 +0000

    initail commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    
+    return winRecord ? winRecord.year : undefined;
+  }
+  
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+  ];
+  
+  console.log(superbowlWin(record)); // Output: "2015"
+  
